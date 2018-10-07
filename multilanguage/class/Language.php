fce91e06157155f5ab024c41ad75d2859b040507
Thomas Deruel <thomas.deruel@hetic.net>
2018-10-07 17:41:22 +0200

    add trim & lowercase at $get

--- multilanguage/class/Language.php
+++ multilanguage/class/Language.php
@@ -2,6 +2,7 @@
 
 class Language{
     static public function getPageLanguage($get){
+        $get = trim(strtolower($get));
         $jsonString = file_get_contents('_lang/'.$get.'.json');
         $parse = json_decode($jsonString);
         return $parse;
